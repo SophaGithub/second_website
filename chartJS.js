804e4f183542fa0f25c8fcb5bfcfd8104b9bce5d
SophaGithub <104049020+SophaMeta@users.noreply.github.com>
2023-05-26 15:09:07 +0700

    change something

--- chartJS.js
+++ chartJS.js
@@ -2,7 +2,7 @@ const ctx1 = document.getElementById('chart-1').getContext('2d');
 const myChart = new Chart(ctx1, {
     type: 'polarArea',
     data: {
-        labels: ['Facebook', 'Youtube', 'Amazon'],
+        labels: ['ភ្នំពេញ', 'បណ្ដាខេត្ត', 'ផ្សេង'],
         datasets: [{
             label: '# of Votes',
             data: [600, 800, 1000],
@@ -24,7 +24,7 @@ const ctx2 = document.getElementById('chart-2').getContext('2d');
 const myChart2 = new Chart(ctx2, {
     type: 'bar',
     data: {
-        labels: ['Facebook', 'Youtube', 'Amazon'],
+        labels: ['ភ្នំពេញ', 'បណ្ដាខេត្ត', 'ផ្សេង'],
         datasets: [
             {
             label: '# of Votes',
